72b1f66ed0bfaf3004f455e92e293661010768e8
Samuel Silva <samuelsilvawb@gmail.com>
2024-01-30 20:53:09 +0000

    test: remove FormData from polyfills as it's conflicting with msw

--- jest.polyfills.js
+++ jest.polyfills.js
@@ -6,14 +6,13 @@ Object.defineProperties(globalThis, {
 });
 
 const { Blob, File } = require("node:buffer");
-const { fetch, Headers, FormData, Request, Response } = require("undici");
+const { fetch, Headers, Request, Response } = require("undici");
 
 Object.defineProperties(globalThis, {
   fetch: { value: fetch, writable: true },
   Blob: { value: Blob },
   File: { value: File },
   Headers: { value: Headers },
-  FormData: { value: FormData },
   Request: { value: Request },
   Response: { value: Response },
 });
