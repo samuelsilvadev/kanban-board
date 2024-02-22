2537261d44be70b4bf24a68d703b7f3dccc0d3da
Samuel Silva <samuelsilvawb@gmail.com>
2024-02-22 22:59:43 +0000

    chore: add data-testid on LogoutButton

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -5,6 +5,7 @@ export function LogoutButton() {
 
   return (
     <button
+      data-testid="logout-button"
       className="border border-slate-700 px-4 py-2 flex-shrink-0  disabled:opacity-50 disabled:bg-red-400 disabled:border-red-400 disabled:cursor-not-allowed"
       onClick={logout}
       disabled={!!error || isLoading}
